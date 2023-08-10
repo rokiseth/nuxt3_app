642807ed4c7fafcc405df1ec05c3a267946783f9
新道 雄太 <shindoh_yu@nisseicom.co.jp>
2023-08-10 15:08:21 +0900

    first commit

--- plugins/sample.ts
+++ plugins/sample.ts
@@ -1,13 +1,17 @@
-class SamplePlugins {
-  public hoge = (): string => {
-    return "hoge";
+export interface SamplePluginInterface {
+  hoge(): string;
+}
+
+class SamplePlugin implements SamplePluginInterface {
+  public hoge = () => {
+    return 'hoge';
   };
 }
 
 export default defineNuxtPlugin((nuxtApp) => {
   return {
     provide: {
-      samp: new SamplePlugins(),
+      samp: new SamplePlugin(),
     },
   };
 });
